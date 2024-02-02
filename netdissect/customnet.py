1345e7240ec6540267465a1c322e4d0d647e380f
VictoriousAnnro <112409949+VictoriousAnnro@users.noreply.github.com>
2024-02-02 12:00:24 +0100

    testing model_urls

--- netdissect/customnet.py
+++ netdissect/customnet.py
@@ -6,7 +6,8 @@ import numpy, torch, math, os
 from torch import nn
 from collections import OrderedDict
 from torchvision.models import resnet
-from torchvision.models.alexnet import model_urls as alexnet_model_urls
+#from torchvision.models.alexnet import model_urls as alexnet_model_urls
+from torchvision.models.alexnet.Alexnet import model_urls as alexnet_model_urls
 
 class CustomResNet(nn.Module):
     '''
